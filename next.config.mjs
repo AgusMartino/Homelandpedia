7fc603942386a97bf5b3b05d42198cd1e403b9a5
AgusMartino <martinoagustin4@gmail.com>
2024-11-27 22:49:57 -0300

    implementacion de patron global de imagenes

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "axiecdn.axieinfinity.com",
       },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
