24039e633c2759fbef37642636300e587ab8c15e
AgusMartino <martinoagustin4@gmail.com>
2024-12-16 12:34:48 -0300

    agregado de google ads

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,7 +7,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer"; // Asegúrate de importar el Footer
+import Footer from "@/components/Footer";
+import AdBanner from "@/components/AdBanner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,27 +24,52 @@ export default function RootLayout({ children }) {
         <AdSense pId="5303334400624183"/>
       </head>
       <body
-        className={`${inter.className} max-w-screen flex flex-col min-h-screen`}
+  className={`${inter.className} max-w-screen flex flex-col min-h-screen`}
+>
+  <InitColorSchemeScript />
+  <CssVarsProvider defaultMode="dark">
+    <CssBaseline />
+
+    {/* Navbar fijo con mayor prioridad */}
+    <div className="fixed top-0 left-0 w-full z-20 bg-white">
+      <Navbar />
+    </div>
+
+    {/* Contenedor del AdBanner con dimensiones específicas */}
+    <div className="relative w-full z-10 bg-white mt-[100px] flex justify-center">
+      <div 
+        className="flex justify-center items-center"
+        style={{
+          minWidth: "300px", // Asegura un ancho mínimo
+          minHeight: "20px", // Asegura una altura mínima
+          width: "600px",
+          height: "60px",
+        }}
       >
-        <InitColorSchemeScript />
-        <CssVarsProvider defaultMode="dark">
-          <CssBaseline />
-          {/* Navbar fijo con altura ajustada */}
-          <div className="fixed top-0 left-0 w-full z-10 bg-white">
-            <Navbar />
-          </div>
+        <AdBanner             
+          dataAdFormat="auto"
+          dataFullWidthResponsive={true}
+          dataAdSlot="6614259860"
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "block",
+          }}
+        />
+      </div>
+    </div>
 
-          {/* Contenedor que permite el crecimiento del contenido y padding inferior */}
-          <div className="mt-28 flex-grow pb-20"> {/* Asegúrate de agregar un padding inferior */}
-            {children}
-          </div>
+    {/* Contenedor principal para los children */}
+    <div className="flex-grow pb-20">
+      {children}
+    </div>
 
-          {/* Footer fijo en la parte inferior */}
-          <div className="fixed bottom-0 left-0 w-full z-20 bg-white"> {/* Asegúrate de que el fondo sea sólido */}
-            <Footer />
-          </div>
-        </CssVarsProvider>
-      </body>
+    {/* Footer fijo en la parte inferior */}
+    <div className="fixed bottom-0 left-0 w-full z-20 bg-white">
+      <Footer />
+    </div>
+  </CssVarsProvider>
+</body>
     </html>
   );
 }
