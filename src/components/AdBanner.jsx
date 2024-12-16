c5f54a83de39517f3d783739dac363d0b9c45f1c
AgusMartino <martinoagustin4@gmail.com>
2024-12-16 12:40:09 -0300

    correccion de componente adBanner

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -1,24 +1,30 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 const AdBanner = ({ dataAdSlot, dataAdFormat, dataFullWidthResponsive }) => {
+  const adRef = useRef(null);
+
   useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (error) {
-      console.log(error.message);
+    // Asegúrate de que el contenedor tenga tamaño visible antes de ejecutar el push
+    if (adRef.current) {
+      try {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      } catch (error) {
+        console.log("Google Ads Error:", error.message);
+      }
     }
   }, []);
 
   return (
     <ins
+      ref={adRef} // Usar ref para asegurar que el contenedor esté disponible
       className="adsbygoogle"
-      style={{ display: "block" }}
+      style={{ display: "block", width: "100%", height: "25px" }} // Ajuste de ancho y alto explícito
       data-ad-client="ca-pub-5303334400624183"
       data-ad-slot={dataAdSlot}
       data-ad-format={dataAdFormat}
-      data-full-width-responsive={dataFullWidthResponsive.toString()}
+      data-full-width-responsive={dataFullWidthResponsive} // No es necesario convertirlo a cadena
     ></ins>
   );
 };
